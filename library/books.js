1829c8bd4a06a840a08d32ce2490a594f84c1d11
kumudlakara <lakara.kumud@gmail.com>
2021-01-05 13:06:49 +0530

    add new card

--- library/books.js
+++ library/books.js
@@ -1,4 +1,4 @@
-let myLibrary = []
+let titles = []
 let b1;
 let update = false;
 
@@ -28,33 +28,55 @@ function addToStorage(b1){
     }
     //console.log(document.querySelector('#title'));
     storage.setItem(b1.title, JSON.stringify(b1));
+    //titles.push(b1.title);
     storage.setItem('totalBooks', parseInt(storage.getItem("totalBooks"))+1);
     storage.setItem('totalPages', parseInt(storage.getItem("totalPages"))+parseInt(b1.npages));
     }
     console.log(b1);
-    cardData(b1);
+    
+    console.log("addtostroage complete");
+    
+    //cardData(b1);
+    //return b1;
 };
 
-function cardData(b1){
+async function cardData(k){
     console.log("in carddata");
+    const b = storage[k];
+
+    let title = b.substring(10, b.indexOf('"author"'));;
+    title = title.substring(0, title.indexOf('"'));
+    let author = b.substring(b.indexOf('"author"') + 10, b.indexOf('"npages"'));
+    author = author.substring(0, author.indexOf('"'));
+    let npages = b.substring(b.indexOf('"npages"') + 10, b.indexOf('"read"'));
+    npages = npages.substring(0, npages.indexOf('"'));
+    let read = b.substring(b.indexOf('"read"') + 8, b.indexOf('"}'));
+    read = read.substring(0, read.indexOf('"'));
+    
     const mainDiv = document.querySelector('#cards');
     const cardDiv = document.createElement('div');
-    cardDiv.classList.add('.card');
+    cardDiv.classList.add('card');
     const btitle = document.createElement('p');
-    const pages = document.createElement('p');
+    const bauthor = document.createElement('p');
+    const bpages = document.createElement('p');
     const complete = document.createElement('p');
     const removebtn = document.createElement('button');
-    const titlenode = document.createTextNode("Book: "+b1.title);
+
+    const titlenode = document.createTextNode("Book: "+title);
     btitle.appendChild(titlenode);
-    pages.innerHTML = "Pages: "+b1.npages;
-    complete.innerHTML = "Complete: "+b1.read;
+    const authornode = document.createTextNode("Author: "+author);
+    bauthor.appendChild(authornode);
+    const pagesnode = document.createTextNode("Pages: "+npages);
+    bpages.appendChild(pagesnode);
+    //complete.innerHTML = "Complete: "+b['read'];
     removebtn.innerHTML = "Remove";
     removebtn.classList.add("title");
     
-    cardDiv.appendChild(btitle, pages, complete, removebtn);
+    cardDiv.append(btitle, bauthor, bpages, removebtn);
     console.log(cardDiv);
     mainDiv.appendChild(cardDiv);
     console.log(mainDiv);
+    console.log("card data complete");
     /*
     const mainDiv = document.querySelector('#sidebar');
     const title = document.createElement('p');
@@ -64,6 +86,7 @@ function cardData(b1){
     */  
 }
 
+
 function addBookToLibrary() {
     if(document.querySelector('#title').value !== "" && document.querySelector('#author').value !== ""
     && document.querySelector('#npages').value !== ""){
@@ -72,12 +95,12 @@ function addBookToLibrary() {
     document.querySelector('#npages').value,
     document.querySelector('#read').value
     );
+    //cardData();
     addToStorage(b1);
-    
     /*
     
     */
-   update = true;
+   console.log("add to lib complete");
     }   
 };
 
@@ -95,7 +118,7 @@ function updateMain(stat){
 
 function removeBook(){
     
-}
+};
 
 function addCard(){
     console.log("in addcard");
@@ -106,7 +129,8 @@ function addCard(){
     console.log("after form");
     const addbtn = document.querySelector('#add-btn');
     addbtn.addEventListener('click', addBookToLibrary);
-}
+    console.log("add card complete");
+};
 
 
 const storage = window.sessionStorage;
@@ -114,8 +138,35 @@ const stats = document.querySelectorAll('.stat');
 stats.forEach(stat => updateMain(stat));
 const showForm = document.querySelector('#show-form-btn');
 const sidebar = document.querySelector("#sidebar");
+console.log(storage);
 showForm.addEventListener('click', addCard);
+const keys = Object.keys(storage);
+console.log(keys);
+function showCards(keys){
+    keys.forEach(key => cardData(key));
+}
+showCards(keys);
 
+/*
+const mainDiv = document.querySelector('#cards');
+    const cardDiv = document.createElement('div');
+    cardDiv.classList.add('card');
+    const btitle = document.createElement('p');
+    const pages = document.createElement('p');
+    const complete = document.createElement('p');
+    const removebtn = document.createElement('button');
+    const titlenode = document.createTextNode("Book: ");
+    btitle.appendChild(titlenode);
+    pages.innerHTML = "Pages: ";
+    complete.innerHTML = "Complete: ";
+    removebtn.innerHTML = "Remove";
+    removebtn.classList.add("title");
+    
+    cardDiv.appendChild(btitle, pages, complete, removebtn);
+    console.log(cardDiv);
+    mainDiv.appendChild(cardDiv);
+    console.log(mainDiv);
+*/
 
 
 
